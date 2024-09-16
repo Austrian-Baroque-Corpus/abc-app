c98f53ed749add1a4131780cb7421e1739724223
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2024-09-16 16:04:20 +0200

    removing keytatic for github static output

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,42 +4,37 @@ import { fields, singleton } from "@keystatic/core";
 export const singletons = {
 	corpus: singleton({
 		label: "Corpus",
-		path: "content/corpus",
 		schema: {
 			title: fields.text({ label: "Title" }),
 		},
 	}),
 	annotation: singleton({
 		label: "Annotation",
-		path: "content/annotation",
 		schema: {
 			title: fields.text({ label: "Title" }),
 		},
 	}),
 	nutzung: singleton({
 		label: "Nutzung",
-		path: "content/nutzung",
 		schema: {
 			title: fields.text({ label: "Title" }),
 		},
 	}),
 	cite: singleton({
 		label: "Zitierung, Kontakt",
-		path: "content/cite",
+		path: "cite",
 		schema: {
 			title: fields.text({ label: "Title" }),
 		},
 	}),
 	ueber_uns: singleton({
 		label: "Über Uns",
-		path: "content/ueber_uns",
 		schema: {
 			title: fields.text({ label: "Title" }),
 		},
 	}),
 	dank: singleton({
 		label: "Dank",
-		path: "content/dank",
 		schema: {
 			title: fields.text({ label: "Title" }),
 		},
