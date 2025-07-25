a242a42204fcfa17d3f34d287137534c74e3eddd
Daniel Elsner <daniel.elsner@oeaw.ac.at>
2025-07-25 09:20:54 +0200

    minro style changes; added hit count all

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,6 @@
-// <reference path="../.astro/types.d.ts" />
-// <reference types="astro/client" />
+// Custom types declarations
+declare let myString: string;
+
+// Astro types, not necessary if you already have a `tsconfig.json`
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
