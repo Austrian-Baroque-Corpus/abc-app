d112daece6f486d132d1a82fd3d00bab50802914
Christian Lendl <contact@dchris.net>
2026-04-20 12:02:53 +0200

    fix: break point for responsive screens (sm to md)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,14 +35,14 @@ module.exports = {
 		},
 
 		screens: {
-			sm: { min: "320px", max: "767px" },
-			// => @media (min-width: 640px and max-width: 767px) { ... }
+			sm: { min: "320px", max: "874px" },
+			// => @media (min-width: 640px and max-width: 874px) { ... }
 
-			md: { min: "768px", max: "1023px" },
-			// => @media (min-width: 768px and max-width: 1023px) { ... }
+			md: { min: "875px", max: "1023px" },
+			// => @media (min-width: 875px and max-width: 1023px) { ... }
 
-			lg: { min: "1024px", max: "1420px" },
-			// => @media (min-width: 1024px and max-width: 1420px) { ... }
+			lg: { min: "1024px", max: "1419px" },
+			// => @media (min-width: 1024px and max-width: 1419px) { ... }
 
 			xl: { min: "1420px", max: "1535px" },
 			// => @media (min-width: 1420px and max-width: 1535px) { ... }
