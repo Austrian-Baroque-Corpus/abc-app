0e7c6bcee20d1ab3f52fe2c1a609dcb3b93e1387
Christian Lendl <contact@dchris.net>
2026-04-20 11:51:03 +0200

    fix: break point for responsive screens (lg to xl)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,11 +41,11 @@ module.exports = {
 			md: { min: "768px", max: "1023px" },
 			// => @media (min-width: 768px and max-width: 1023px) { ... }
 
-			lg: { min: "1024px", max: "1279px" },
-			// => @media (min-width: 1024px and max-width: 1279px) { ... }
+			lg: { min: "1024px", max: "1420px" },
+			// => @media (min-width: 1024px and max-width: 1420px) { ... }
 
-			xl: { min: "1280px", max: "1535px" },
-			// => @media (min-width: 1280px and max-width: 1535px) { ... }
+			xl: { min: "1420px", max: "1535px" },
+			// => @media (min-width: 1420px and max-width: 1535px) { ... }
 
 			"2xl": { min: "1536px" },
 			// => @media (min-width: 1536px) { ... }
